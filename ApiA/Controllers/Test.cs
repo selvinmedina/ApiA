c243ce958a23ccded2be3a2817481770bfa73bf5
Selvin Medina <selvinmedina0016@gmail.com>
2021-11-12 22:47:01 -0600

    Agregar archivos de proyecto.

---
+++ ApiA/Controllers/Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiA.Controllers
+{
+    [ApiController]
+    [Route("Test")]
+    public class Test : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<Test> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Test(ILogger<Test> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("ObtenerSaludo")]
+        public IActionResult ObtenerSaludo()
+        {
+            var saludo = _configuration["SALUDO"] ?? "No hay saludo";
+
+            return Ok(saludo);
+        }
+    }
+}
